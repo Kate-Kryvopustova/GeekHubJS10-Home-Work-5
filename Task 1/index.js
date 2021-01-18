1725ee83f4ccdd6edebf51f2b2376f95db0ca6c9
Kate-Kryvopustova <k.kryvopustova@gmail.com>
2021-01-18 23:29:01 +0200

    Fixed function perform

--- Task 1/index.js
+++ Task 1/index.js
@@ -1,17 +1,59 @@
-function perform(val, callBack) {
-  return Promise.resolve(callBack(val));
+// function perform() {
+//   TODO implement
+// }
+// perform(20, function (value) {
+//   console.log(value) // 20
+//   var param = 1;
+//   console.log(param); // 1
+//   return param;
+// })
+//   .then('a', 'b', function (a, b, param) {
+//     console.log(++param); // 2
+//     return param;
+//   })
+//   .then(function (param) { // param === 2
+//     console.log(++param); // 3
+//     return param;
+//   });
+
+
+function perform(props, callback) {
+  let param = callback(props);
+
+  function then(...args) {
+    const callback = args.find( elem => typeof (elem) === "function" );
+    const params = args.filter( elem => typeof (elem) !== 'function' );
+
+    param = callback(...params, param);
+    return { then };
+  }
+
+  return { then };
 }
-perform(20, function(value) {
-  console.log(value)
+
+perform(20, function (value) {
+  console.log(value);
   var param = 1;
   console.log(param);
   return param;
 })
-.then(function(param) {
-  console.log(++param);
-  return param;
-})
-.then(function(param) {
-  console.log(++param); 
-  return param;
-})
+  .then('a', 'b', function (a, b, param) {
+    console.log(++param);
+    return param;
+  })
+  .then(function (param) {
+    console.log(++param); 
+    return param;
+  })
+  .then(function (param) {
+    console.log(++param); 
+    return param;
+  })
+  .then(function (param) {
+    console.log(++param); 
+    return param;
+  })
+  .then(function (param) {
+    console.log(++param); 
+    return param;
+  });
